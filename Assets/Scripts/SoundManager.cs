b7cad9777dd2c9458f1f46f954a3a5f7fe979d4f
Lucas Teles <lucas.teles@lambda3.com.br>
2020-06-22 11:42:43 -0300

    wip

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField] AudioClip hitCharacter;
+    [SerializeField] AudioClip hitOther;
+    [SerializeField] AudioClip shotArrow;
+    [SerializeField] AudioClip die;
+    [SerializeField] AudioClip jump;
+
+    [SerializeField] AudioSource source;
+
+    public static SoundManager Instance;
+    
+    void Awake() => Instance = this;
+
+    void Play(AudioClip clip)
+    {
+        source.Stop();
+        source.PlayOneShot(clip);
+    }
+
+    public void PlayHitChatacter() => Play(hitCharacter);
+    
+    public void PlayHitOther()=> Play(hitOther);
+
+    public void PlayShot() => Play(shotArrow);
+    
+    public void PlayDie() => Play(die);
+    public void PlayJump() => Play(jump);
+
+}
